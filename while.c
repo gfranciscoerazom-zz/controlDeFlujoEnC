b6c9711d02e19246bfb7e14cc5b1d4ce8035d3c2
gfranciscoerazom <gfranciscoerazom@protonmail.com>
2021-08-05 17:47:48 -0500

    Uso de while

---
+++ while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n = 20;
+    while (n > 10) {
+        printf("n es igual a %d\n", n);
+        n--;
+    }
+    printf("Hemos salido de lbucle porqeu n es igual o menor que 10");
+    
+    return 0;
+}
